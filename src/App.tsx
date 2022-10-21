a477519dba989e8586398374dd025489f4515975
Seungwoo Park <qpfgud430@gmail.com>
2022-10-21 16:40:24 +0900

    feat: Create animate

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(135deg, #e09, #d0e);
+`;
+
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 15px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const myVars = {
+  start: { scale: 0 },
+  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+};
+
+function App() {
+  return (
+    <Wrapper>
+      <Box variants={myVars} initial="start" animate="end" />
+    </Wrapper>
+  );
+}
+
+export default App;
