04d75d845264babd0068bf1878164ea0382f14f6
Seungwoo Park <qpfgud430@gmail.com>
2022-11-13 21:21:56 +0900

    Create Search function

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,12 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
 import { useLocation } from "react-router-dom";
+import { getSearch } from "../../Api/api";
+import Loading from "../../Styles/Loading";
 
+const Body = styled.div`
+  font-family: "Raleway Sans";
+  width: 100%;
+  height: 100%;
+  position: relative;
+`;
 
 function Search() {
   const location = useLocation()
   const keyword = new URLSearchParams(location.search).get("keyword");
+  const {data, isLoading}  = useQuery("search", ()=> getSearch(query + ""));
   return (
     <>
+    {isLoading ? (
+      <Loading/>
+    ):(
+      <Body>
+
+      </Body>
+    )}
     </>
   )
 }
